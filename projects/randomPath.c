0a499d8a2adedd39aeea51890a09984cb7315c2c
Danny <dlhammer2@catamount.wcu.edu>
2019-12-26 14:25:32 -0500

    Updated randomPath.c

--- projects/randomPath.c
+++ projects/randomPath.c
@@ -27,6 +27,7 @@
 
 int main(void)
 {
+
     // count to iterate through the array of letters
     // direction to determine where the program will move
     // x and y coordinates to determine the current position
@@ -35,28 +36,20 @@ int main(void)
     int count, direction, x, y, canMove, size;
     
     // Determines the size of the grid via user input
-    while ((size % 2) != 1)
-    {
-        printf("Enter an odd number for the size of the grid\n> ");
-        scanf("%d", &size);
-    }
+    printf("Enter a number for the size of the grid\n> ");
+    scanf("%d", &size);
     printf("\nAttempting to generate a randomized path from A to Z\n");
 
     // Two dimensional array representing the grid
     char grid[size][size];
-    
-    // Array of possible letters to make up the path
-    char letters[26] = {'A','B','C','D','E','F',
-        'G','H','I','J','K','L','M','N','O','P',
-        'Q','R','S','T','U','V','W','X','Y','Z'};
-
-    // Set the count to 0 and the coordinates to the center of the grid
-    count = 0;
-    x = CENTER;
-    y = CENTER;
 
     // Setup the rand() function
     srand((unsigned) time(NULL));
+
+    // If the grid is odd, the path begins in the center
+    // If the grid is even, the path begins in the top left corner
+    x = size % 2 == 1 ? CENTER : 0;
+    y = size % 2 == 1 ? CENTER : 0;
 
 
 
@@ -66,18 +59,6 @@ int main(void)
     {
         for (int j = 0; j < size; j++)
         {
-            // Prints all four edges of the grid as lines
-            if (i == 0 || i == (size - 1))
-            {
-                grid[i][j] = '=';
-            }
-            else if (j == 0 || j == (size - 1))
-            {
-                grid[i][j] = '|';
-            }
-            
-            // Prints the center of the grid as dots
-            else
             {
                 grid[i][j] = '.';
             }
@@ -87,24 +68,24 @@ int main(void)
 
 
 
-    //Sets the current position to the current letter
-    grid[y][x] = letters[count];
-    count++;
+    //Sets the current position to the first letter
+    grid[y][x] = 'A';
 
-    while (count < 26)
+    for (count = 1; count < 26;)
     {
         // If there is at least one legal position to move,
         // allow the program to make a movement
-        if (grid[y + 1][x] == '.' || grid[y - 1][x] == '.'
-                || grid[y][x + 1] == '.' || grid[y][x - 1] == '.')
+        if ((grid[y + 1][x] == '.' && (y + 1) < size)
+                || (grid[y - 1][x] == '.' && (y - 1) >= 0)
+                || (grid[y][x + 1] == '.' && (x + 1) < size)
+                || (grid[y][x - 1] == '.' && (x - 1) >= 0))
         {
             canMove = 1;
         }
         // If not, stop the program
         else
         {
-            printf("\nNo legal path was found after '%c'\n",
-                    letters[count - 1]);
+            printf("\nNo legal path was found after '%c'\n", 'A' + count - 1);
             break;
         }
 
@@ -122,40 +103,37 @@ int main(void)
             // If the movement is within the bounds of the grid
             // AND the new space is unmarked (denoted by being '.'),
             // then assign the next letter to the new space,
-            // increase the counter,
             // and exit the movement loop
             switch (direction)
             {
                 case DOWN: if ((y + 1) < size && grid[y + 1][x] == '.')
                         {
-                            grid[++y][x] = letters[count];
-                            count++;
+                            grid[++y][x] = 'A' + count;
                             canMove = 0;
                         }
                          break;
                 case UP: if ((y - 1) >= 0 && grid[y - 1][x] == '.')
                         {
-                            grid[--y][x] = letters[count];
-                            count++;
+                            grid[--y][x] = 'A' + count;
                             canMove = 0;
                         }
                         break;
                 case RIGHT: if ((x + 1) < size && grid[y][x + 1] == '.')
                         {
-                            grid[y][++x] = letters[count];
-                            count++;
+                            grid[y][++x] = 'A' + count;
                             canMove = 0;
                         }
                         break;
                 case LEFT: if ((x - 1) >= 0 && grid[y][x - 1] == '.')
                         {
-                            grid[y][--x] = letters[count];
-                            count++;
+                            grid[y][--x] = 'A' + count;
                             canMove = 0;
                         }
                         break;
             }
         }
+        // A move has been made, so increase the counter
+        count++;
     }
 
     // Print the finalized grid
